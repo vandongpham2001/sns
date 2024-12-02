672caa10da95706a083ac08cc285f759fd891604
vandongpham2001 <vandongpham2001@gmail.com>
2024-12-02 23:58:52 +0700

    feat: init source

---
+++ post-service/src/test/java/com/dongpv/sns/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongpv.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sns-api/src/main/java/com/dongpv/sns/SnsApiApplication.java
@@ -0,0 +1,13 @@
+package com.dongpv.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsApiApplication.class, args);
+	}
+
+}
